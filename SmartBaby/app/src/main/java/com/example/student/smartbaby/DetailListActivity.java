489ff1b74acbb24d27f25c5b831dafb186fd03a2
bkdoo <kyeongdoos@gmail.com>
2019-01-03 15:56:00 +0900

    tet
    123

--- SmartBaby/app/src/main/java/com/example/student/smartbaby/DetailListActivity.java
+++ SmartBaby/app/src/main/java/com/example/student/smartbaby/DetailListActivity.java
@@ -10,5 +10,6 @@ public class DetailListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_list);
         //123
+        //123
     }
 }
