82bce809ec30c56d1e4d503e31bf6f3a4b4c067a
bydk <exmail@naver.com>
2019-01-04 16:53:54 +0900

    대기화면(wait activity)
    wait 엑티비티를 초기화면으로 바꿈
    wait 엑티비티 2초후 login 액티비티로 이동

--- SmartBaby/app/src/main/java/com/example/student/smartbaby/WaitActivity.java
+++ SmartBaby/app/src/main/java/com/example/student/smartbaby/WaitActivity.java
@@ -1,5 +1,7 @@
 package com.example.student.smartbaby;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,20 @@ public class WaitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wait);
+
+        startWaiting();
+    }
+
+    private void startWaiting() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
     }
 }
