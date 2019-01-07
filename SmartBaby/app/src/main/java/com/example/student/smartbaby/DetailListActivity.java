390223fa6650cbf33826596999efe9e7646ad162
bydk <exmail@naver.com>
2019-01-07 13:50:26 +0900

    layout 2차 완성, activity(login,main,detaillist)
    layout 2차 완성, activity(login,main,detaillist)

--- SmartBaby/app/src/main/java/com/example/student/smartbaby/DetailListActivity.java
+++ SmartBaby/app/src/main/java/com/example/student/smartbaby/DetailListActivity.java
@@ -2,14 +2,29 @@ package com.example.student.smartbaby;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class DetailListActivity extends AppCompatActivity {
+
+    TextView tv_date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_list);
-        //123
-        //123
+
+        tv_date = (TextView)findViewById(R.id.tv_date);
+
+        tv_date.setText(Calendar.getInstance().get(Calendar.YEAR) +
+
+                "-" + (Calendar.getInstance().get(Calendar.MONTH) + 1) +
+
+                "-" + Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+
+
     }
 }
--- SmartBaby/app/src/main/java/com/example/student/smartbaby/MainActivity.java
+++ SmartBaby/app/src/main/java/com/example/student/smartbaby/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.student.smartbaby;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_setUp, btn_memo, btn_date,btn_dsleep,btn_nsleep, btn_sleep;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_setUp = (Button)findViewById(R.id.btn_setUp);
+        btn_memo = (Button)findViewById(R.id.btn_memo);
+        btn_date = (Button)findViewById(R.id.btn_date);
+        btn_dsleep = (Button)findViewById(R.id.btn_dsleep);
+        btn_nsleep = (Button)findViewById(R.id.btn_nsleep);
+        btn_sleep = (Button)findViewById(R.id.btn_sleep);
+
+        btn_memo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DetailListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btn_setUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SetupActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
 }
